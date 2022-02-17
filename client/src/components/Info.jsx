a19bdc496903bc7476bf4d885f3091acab5017fa
Kirill Serebrykov <kirill.baza22@gmail.com>
2022-02-17 21:57:37 +0300

    style

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -2,13 +2,21 @@ import React, { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { saveColor } from '../common/SetColor';
+
+const styleBox = {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingLeft: "10px",
+    position: "relative"
+}
+
 const Info = ({ data, name, dynamicColor }) => {
     const [prevValue, setValue] = useState("")
     const [Color, setColor] = useState("")
     // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => saveColor(prevValue, setValue, setColor, data), [data])
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'row', paddingLeft: "10px", position: "relative" }}>
+        <Box sx={styleBox}>
             <Typography variant="subtitle1" sx={{ position: "relative", left: "10px", fontWeight: "700" }} component="div">
                 {name}:
             </Typography>
